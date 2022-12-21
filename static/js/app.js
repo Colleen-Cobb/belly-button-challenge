3e0bd6f499a37f06a8725ed5d907b20d6f9733f6
Colleen-Cobb <colleen.cobb5@gmail.com>
2022-12-21 18:22:49 -0500

    producing charts

--- static/js/app.js
+++ static/js/app.js
@@ -1,82 +1,120 @@
-// read in the samples.json 
 
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+const dataPromise = d3.json(url)
+console.log("Data Promise: ", dataPromise);
+
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+    console.log(data.names);
+
+    let names = data.names;
+
+    let metadata = data.metadata;
+
+    let samples = data.samples;
+  });
+
+// Set up the base chart
 function charts(selectedPatientID) {
     d3.json("samples.json").then((data) => {
-      let plottingData = data.samples;
-      let subject = plottingData.filter(
+      var plottingData = data.samples;
+      var subject = plottingData.filter(
         (sampleobject) => sampleobject.id == selectedPatientID
       )[0];
-
+  
       console.log(subject);
-      let ids = subject.otu_ids;
-      let labels = subject.otu_labels;
-      let values = subject.sample_values;
-      
-      // Trace for the sample data
-      let trace1 = {
+      var ids = subject.otu_ids;
+      var labels = subject.otu_labels;
+      var values = subject.sample_values;
+  
+      // Horizontal Bar Char
+      var trace1 = {
         x: values.slice(0, 10).reverse(),
         y: ids
-            .slice(0, 10)
-            .map((otuID) => `OTU ${otuID}`)
-            .reverse(),
+          .slice(0, 10)
+          .map((otuID) => `OTU ${otuID}`)
+          .reverse(),
         text: labels.slice(0, 10).reverse(),
         type: "bar",
         orientation: "h",
-  };
+      };
+  
+      var data = [trace1];
+  
+      var layout = {
+        title: "Top 10 OTUs",
+        xaxis: { autorange: true },
+        yaxis: { autorange: true },
+        margin: { t: 40, l: 100 },
+        height: 400,
+      };
+  
+      Plotly.newPlot("bar", data, layout);
+  
+      //Bubble Chart 
 
-        // Data trace array
-        let traceData= [trace1];
+      var trace2 = {
+        x: ids,
+        y: values,
+        text: labels,
+        mode: "markers",
+        marker: {
+          color: ids,
+          size: values,
+          colorscale: "plasma",
+        },
+      };
+  
+      var data = [trace2];
+  
+      var layout = {
+        margin: { t: 0 },
+        xaxis: { title: "OTU ID" },
+        hovermode: "closest",
+        width: window.width,
+      };
+  
+      Plotly.newPlot("bubble", data, layout);
+    });
+  }
+  
+  // Demographic Info
+  function demo(selectedPatientID) {
+    d3.json("samples.json").then((data) => {
+      var MetaData = data.metadata;
+      var subject = MetaData.filter(
+        (sampleobject) => sampleobject.id == selectedPatientID
+      )[0];
+      var demographicInfoBox = d3.select("#sample-metadata");
+      demographicInfoBox.html("");
+      Object.entries(subject).forEach(([key, value]) => {
+        demographicInfoBox.append("h5").text(`${key}: ${value}`);
+      });
 
-        let layout = {
-            title: "Top 10 OTUs",
-            xaxis: {autrorange: true}, 
-            yaxis: {autorange: true},
-            margin: {t: 70, 1: 100},
-            hieght: 380,
-        };
-
-        Plotly.newPlot("plot", traceData, layout);
-
-        // Create bubble chart
-
-        let trace2 = {
-            x: ids, 
-            y: values, 
-            text: labels, 
-            mode: "markers", 
-            marker: {
-                color: ids, 
-                size: values, 
-                colorscale: "Plasma",
-            }, 
-        };
-
-        let traceData2= [trace2];
-
-        let layout2 = {
-            margin: { t: 0},
-            xaxis: {title: "OTU ID", 
-            hovermode: "closest",
-            width: window. width, 
-        };
-
-        Plotly.newPlot("bubble", traceData2, layout2);
-    };
-}
-
-// Initialize demographic info
-
-function demoInfo(selectedPatientID) {
-    d3.json("samples.json").then(((data)) => {
-        let metaData = data.metadata;
-        let subject= metaData.filter(
-            (sampleobject) => sampleobject.id == selectedPatientID
-        )[0];
-        let demoInfoBox = d3.select("#sample-metadata");
-        demoInfoBox.html("");
-        Object.entereis(subject).forEach(([key, value]) => {
-            demoInfoBox.append("h5").text(`${key}: ${value}`);
-        });
-
-    },
-};
+    });
+  }
+  
+  // Call the data into the inspector console. 
+  function init() {
+    d3.json("samples.json").then(function (data) {
+      console.log("samples.json:", data);
+      // Set up the DropDown:
+      let DropDown = d3.select(`#selDataset`);
+  
+      data.names.forEach((name) => {
+        DropDown.append(`option`).text(name).property(`value`, name);
+      });
+      // Reset demographic info and visuals to first subject when page is refreshed.
+      const firstSample = data.names[0];
+      charts(firstSample);
+      demo(firstSample);
+    });
+  }
+  // Pull data for new subject into demo and visuals. 
+  function optionChanged(newSample) {
+    charts(newSample);
+    demo(newSample);
+  }
+  
+  init();
